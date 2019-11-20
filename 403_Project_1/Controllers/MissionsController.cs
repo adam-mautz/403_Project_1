cae19cb9e6d37cc17dea5b13d01d5754a2a7b04a
Adam.mautz <Adam.mautz@gmail.com>
2019-11-20 16:42:48 -0700

    added Missions controller.

---
+++ 403_Project_1/Controllers/MissionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _403_Project_1.Controllers
+{
+    public class MissionsController : Controller
+    {
+        // GET: Missions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
